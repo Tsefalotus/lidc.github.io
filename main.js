8f9d7f01ea244bc014b336f67b436936be86f09a
Tsefalotus <diana.krauk@gmail.com>
2025-09-05 15:26:36 +0200

    444

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const videos = document.querySelectorAll(".lazy-video");
+  
+    const observerOptions = {
+      root: null, // Отслеживаем относительно viewport
+      threshold: 0.5 // 50% элемента должно быть видно
+    };
+  
+    const videoObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const video = entry.target;
+  
+        if (entry.isIntersecting) {
+          // Если видео пересекает 50% экрана, воспроизводим его
+          video.play();
+        } else {
+          // Если видео выходит из зоны видимости, ставим на паузу
+          video.pause();
+        }
+      });
+    }, observerOptions);
+  
+    // Наблюдаем за каждым видео
+    videos.forEach((video) => {
+      videoObserver.observe(video);
+    });
+  });
+  
